dd2aa99cbb70796d684149a087832451d19e402e
Pranav Aher <prranavyaher@gmail.com>
2022-04-30 12:13:50 +0530

    button ui modified

--- src/components/Custom-Button/CustomButton.js
+++ src/components/Custom-Button/CustomButton.js
@@ -2,13 +2,24 @@ import React from "react";
 import "./CustomButton.scss";
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
   return (
-    <div
-      className={` ${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+    <button
+      className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
       {...otherProps}
     >
       {children}
-    </div>
+    </button>
   );
 };
 
 export default CustomButton;
+
+// const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+//   <button
+//     className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+//     {...otherProps}
+//   >
+//     {children}
+//   </button>
+// );
+
+// export default CustomButton;
